9c3b4ae835ecc6b30bbba65563a2ba1634e6096f
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-07-03 11:09:53 +0530

    chore: Refactor navigation menu in layout component
    The code changes refactor the navigation menu in the layout component to improve its structure and readability. The unnecessary commented-out code is removed, and the correct syntax for the navigation links is added. This ensures that the navigation menu is properly displayed on the website.
    
    Note: The recent user commit "Set up a fresh Laravel app" is not relevant to these code changes.

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,1 +1,5 @@
-<a href="/{{ $slot == 'Home' ? '' : strtolower($slot) }}"> {{ $slot }} </a> |
+<!-- This was somthing I did after Video 3  assignment -->
+<!-- <a href="/{{ $slot == 'Home' ? '' : strtolower($slot) }}"> {{ $slot }} </a> | -->
+
+<!-- Correction after Video 4 -->
+<a {{ $attributes }}> {{ $slot }} </a>
