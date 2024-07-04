2014ffcf2184e6df9787a5ff4a7dc1802ef5f754
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-07-04 11:03:33 +0530

    refactor: Improve navigation menu structure and readability
    Add new pages for About and Contact

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-slot:headingSlot> About page Heading</x-slot:headingSlot>
     <h1>Welcome to About</h1>
     <p>This is the content of the welcome page.</p>
 </x-layout>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-slot:headingSlot> Home page Heading</x-slot:headingSlot>
     <h1>Welcome to Home</h1>
     <p>This is the content of the welcome page.</p>
 </x-layout>
