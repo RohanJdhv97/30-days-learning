20c16f11e882ba4fa32ef265e5dda628c76c0a62
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-07-18 02:38:09 +0530

    refactor: Update Jobs model and routes for better code organization
    The code changes refactor the Jobs model and routes to improve code organization. The Jobs model is moved to the "app/Models" directory for better separation of concerns. The routes in "routes/web.php" are updated to use the Jobs model methods instead of hardcoding the job data. This ensures better maintainability and scalability of the codebase.
    
    Note: The recent user commit "Set up a fresh Laravel app" is not relevant to these code changes.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Jobs;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
@@ -11,34 +13,10 @@ Route::get('/about', function () {
 });
 
 Route::get('/jobs', function () {
-    return view('jobs', [
-        "jobs" => [
-            [
-                'id' => '1',
-                'title' => 'Web Developer',
-                'pay' => '5,00,000'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Graphic Designer',
-                'pay' => '10,00,000'
-            ]
-        ]
-    ]);
+    return view('jobs', ['jobs'=> Jobs::all()]);
 });
 Route::get('/job/{id}', function ($id) {
-    $jobs = [
-        [
-            'id' => '1',
-            'title' => 'Web Developer',
-            'pay' => '5,00,000'
-        ],
-        [
-            'id' => '2',
-            'title' => 'Graphic Designer',
-            'pay' => '10,00,000'
-        ]
-    ];
-    $job = Arr::first($jobs, fn ($job) => $job['id'] == $id);
-    return view('jobs',$job);
+
+    $jobData = Jobs::find($id);
+    return view('job', ["job" => $jobData]);
 });
