d26bedd02cdf04332a2fd030c4545d0161a4df67
RohanJdhv97 <rohan.jadhav@zysk.tech>
2024-07-27 00:12:48 +0530

    refactor: Add employer_id to Job model fillable array

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,7 +12,7 @@ class Job extends Model
 {
     use HasFactory;
     protected $table = 'job_listings';
-    protected $fillable = ['title', 'salary'];
+    protected $fillable = ['title', 'salary', 'employer_id'];
 
     public $timestamps = true;
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use App\Models\Job;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
@@ -13,11 +14,26 @@ Route::get('/about', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->simplePaginate(3);
-    return view('jobs', ['jobs' => $jobs]);
+    $jobs = Job::with('employer')->latest()->simplePaginate(3);
+    return view('jobs.index', ['jobs' => $jobs]);
 });
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+});
+
+Route::post('/jobs',function(){
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => Employer::first()->id,
+    ]);
+
+    return redirect('/jobs');
+});
+
 Route::get('/job/{id}', function ($id) {
 
     $jobData = Job::find($id);
-    return view('job', ["job" => $jobData]);
+    return view('jobs.show', ["job" => $jobData]);
 });
